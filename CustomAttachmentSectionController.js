a1d122f68c4fbfb5bc7df6fbffdfbef30ab8ef51
Pruthvi Ponnaganti <40760060+pruthvi04@users.noreply.github.com>
2023-03-03 15:07:54 +0530

    CustomAttachmentSectionController.js
    Controller JavaScript file

---
+++ CustomAttachmentSectionController.js
@@ -0,0 +1,59 @@
+({  
+    doInit:function(component,event,helper){
+        
+        var actions = [
+            { label: 'Open File', name: 'toOpen' }
+        ]
+        component.set('v.testColumns', [
+            {label: 'Title', fieldName: 'linkTitle', type: 'url', 
+             typeAttributes: {label: { fieldName: 'Title' }, target: '_blank'}},
+            {label: 'Type', fieldName: 'FileType', type: 'text'},
+            {label: 'Created By', fieldName: 'linkCreatedBy', type: 'text', 
+             /*typeAttributes: {label: { fieldName: 'CreatedBy.Name' }, target: '_blank'}*/},
+            {label: 'Last Modified', fieldName: 'LastModifiedDate', type: 'DateTime'},
+            {label: '', type: 'action', typeAttributes: { rowActions: actions }},
+        ]);
+        
+        var action = component.get("c.getFiles");  
+        action.setParams({  
+            "recordId":component.get("v.recordId")  
+        });      
+        action.setCallback(this,function(response){  
+            var state = response.getState();  
+            if(state=='SUCCESS'){  
+                var result = response.getReturnValue();  
+                console.log('result: ' +result.cdList);
+                result.cdList.forEach(function(record){
+                    console.log('record::'+JSON.stringify(record));
+                    record.linkTitle = '/'+record.Id;
+                    record.linkCreatedBy = record.CreatedBy.Name;
+                 });
+            } 
+            component.set("v.files",result.cdList);
+            component.set("v.recordsCount",result.cdCount);
+        });  
+        $A.enqueueAction(action);
+    },
+    //to open the file
+    OpenFile :function(component,event,helper){  
+        var action = event.getParam('action');
+        var row = event.getParam('row');
+        var rec_id = row.Id;
+        $A.get('e.lightning:openFiles').fire({ //Lightning Openfiles event  
+            recordIds: [rec_id] //file id  
+        });  
+    }, 
+    
+    UploadFinished : function(component, event, helper) {  
+        var uploadedFiles = event.getParam("files");  
+        var documentId = uploadedFiles[0].documentId;  
+        var fileName = uploadedFiles[0].name;  
+        helper.UpdateDocument(component,event,documentId);  
+        var toastEvent = $A.get("e.force:showToast");  
+        toastEvent.setParams({  
+            "title": "Success!",  
+            "message": "File "+fileName+" Uploaded successfully."  
+        });  
+        toastEvent.fire();  
+    },  
+})
